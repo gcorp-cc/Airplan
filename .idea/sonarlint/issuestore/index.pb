4abd4eb686556afc310e41346e101eb5b2559557
GcorpHosseinRad <hosseinradvictor@gmail.com>
2020-03-01 11:53:48 +0330

    Start singel Page AirPlane Web

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,4 @@
+
+:
+
+js/init.js,4\9\49e795133e49c5fa27ed2ef36d87f72c5d2ce508
---
+++ js/init.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.slider').slider();
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+    $('.modal').modal();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+$(document).ready(function(){
+    $('.tabs').tabs();
+});
+$(document).ready(function(){
+    $('.dropdown-trigger').dropdown();
+});
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
